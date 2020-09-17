bb8e8bb3c857b38e707776ba508b45c3647651ec
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-17 13:45:13 +0200

    Player statistics programma gemaakt

---
+++ PYTB1L2PlayerStatistics/Statistics.py
@@ -0,0 +1,28 @@
+name = 'Zoezo'
+
+playerHealth = 90
+playerStamina = 40
+
+primaryGun = 'Heckler & Koch MP5'
+secondaryGun = 'M1911'
+
+wanted = True
+bounty = 1500000
+
+companion = 'Dogmeat'
+companionHealth = 100
+companionStamina = 50
+
+print('Name: ' + name)
+print('Health: ' + str(playerHealth))
+print('Stamina: ' + str(playerStamina))
+print()
+print('Primary gun: ' + primaryGun)
+print('Secondary gun: ' + secondaryGun)
+print()
+print('Wanted: ' + str(wanted))
+print('Bounty: ' + str(bounty))
+print()
+print('Companion: ' + companion)
+print('Companion\'s health: ' + str(companionHealth))
+print('Companion\'s stamina: ' + str(companionStamina))
