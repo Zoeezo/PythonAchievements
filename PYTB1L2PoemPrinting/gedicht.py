a9fc565fe9fea73a8bdf0c56c07b789e45f881d7
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-17 13:34:38 +0200

    Gedicht gemaakt

---
+++ PYTB1L2PoemPrinting/gedicht.py
@@ -0,0 +1,4 @@
+vers1 = 'Ik ben zoey bosman. Ik doe vaak dingen zonder een plan. Ik woon in spierdijk. Ik ben niet heel rijk. ' # ik moest iets... lol
+vers2 = 'Ik hou van programmeren. Ik wil er graag zoveel mogelijk over leren. Ik heb een hekel aan warm weer, ik explodeer.'
+
+print(vers1 + vers2)
