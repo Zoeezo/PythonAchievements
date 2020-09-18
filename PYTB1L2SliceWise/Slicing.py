b3bfcf1582bce802dbccca04d67187b78192f4f9
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-18 14:38:19 +0200

    string splicing

---
+++ PYTB1L2SliceWise/Slicing.py
@@ -0,0 +1,24 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+
+print(tekst[37:-25])
+print(tekst[43:-18])
+print(tekst[61:])
+print()
+
+tekst = 'SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ...'
+
+print(tekst[15:-71])
+print(tekst[23:-66])
+print(tekst[28:-44])
+print(tekst[52:-21])
+print()
+
+tekst = 'Wat is hier het laatste woord?'
+
+print(tekst[-6:-1])
+print()
+
+tekst = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+
+print(tekst[5:9])
+print(tekst[29:33])
