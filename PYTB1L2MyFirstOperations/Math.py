f664d500cb048e038504183cf2dc2f475c5e01a0
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-18 13:56:13 +0200

    Math script gemaakt

---
+++ PYTB1L2MyFirstOperations/Math.py
@@ -0,0 +1,16 @@
+var1 = 100
+var2 = 5
+
+var3 = var1 + var2
+var4 = var2 - var1
+
+var5 = var4 / var2
+var6 = var3 % var2
+
+var6 += var4
+
+var6 /= var5
+
+finalVar = var6 * 4.54
+
+print(finalVar)
