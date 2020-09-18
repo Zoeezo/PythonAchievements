93216503a8be05debd8b9477d504052f940eddd2
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-18 13:49:59 +0200

    Apples gemaakt

---
+++ PYTB1L2HowYouLikeThemApples/apples.py
@@ -0,0 +1,21 @@
+appelbomen = 333
+schaduwBomen = appelbomen / 3 * 2
+zonBomen = appelbomen - schaduwBomen
+
+print('Totale bomen: ' + str(appelbomen))
+print('Schaduw bomen: ' + str(schaduwBomen))
+print('Zon bomen: ' + str(zonBomen) + '\n')
+
+zonAppels = 146
+schaduwAppels = zonAppels * 0.8
+
+print('Schaduw appels: ' + str(schaduwAppels))
+print('Zon appels: ' + str(zonAppels) + '\n')
+
+totaleAppels = (zonBomen * zonAppels) + (schaduwBomen * schaduwAppels)
+persoonlijkeAppels = int(totaleAppels / 4)
+
+print('Totale appels: ' + str(totaleAppels))
+print('Persoonlijke appels: ' + str(persoonlijkeAppels) + '\n')
+
+print('Je mag ' + str(persoonlijkeAppels) + ' appels verkopen.')
