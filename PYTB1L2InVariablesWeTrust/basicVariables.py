110d6185489cf13265935ae91f66a35171d0f2cb
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-18 14:50:41 +0200

    Basic variables geleerd

---
+++ PYTB1L2InVariablesWeTrust/basicVariables.py
@@ -0,0 +1,19 @@
+char1 = 'c'
+float1 = 6.9
+
+bool1 = True
+bool2 = False
+
+int1 = 5
+int2 = 420
+int3 = int1 - int2
+
+string1 = "hoi hoi! "
+string2 = "dag dag! "
+string3 = string1 + string2
+
+print(char1)
+print(float1)
+print(bool2)
+print(int3)
+print(string3)
