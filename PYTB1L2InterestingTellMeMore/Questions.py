282075c2979ee0bd59082144bb1c7e49e423c741
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-18 14:05:09 +0200

    Python vragenlijst gemaakt

---
+++ PYTB1L2InterestingTellMeMore/Questions.py
@@ -0,0 +1,10 @@
+print("Hoe heet je?")
+name = input(">>> ")
+
+print("Hoe oud ben je?")
+age = input(">>> ")
+
+print("Waar woon je?")
+livingPlace = input(">>> ")
+
+print("Hallo, " + name + ". Je bent dus " + age + " jaar oud en je woont in " + livingPlace + "!")
