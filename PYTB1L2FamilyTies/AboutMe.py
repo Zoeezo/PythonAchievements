92994f4a8d7c589e3bab10b51633824fe11d0713
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-09-18 14:14:35 +0200

    Verhaaltje over mij in python

---
+++ PYTB1L2FamilyTies/AboutMe.py
@@ -0,0 +1,7 @@
+name = "Zoey" # string
+age = 16 # int
+livingPlace = "Spierdijk" # string
+distance = 56.4 # float
+cats = 4 # int
+
+print("Ik ben " + name + ", ik ben " + str(age) + " jaar oud. Ik woon in " + livingPlace + ", dat is " + str(distance) + " km van school vandaan. Ik heb ook " + str(cats) + " katten!")
