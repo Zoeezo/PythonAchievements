a3e65140b466b8ca15ce2479cc6d65c712ec1222
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2020-10-30 10:31:45 +0100

    Fix #5

--- PYTB1L4WheresWaldo/WheresWaldo.py
+++ PYTB1L4WheresWaldo/WheresWaldo.py
@@ -3,18 +3,20 @@ import random
 people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
 random.shuffle(people)
 
-def findWaldo():
+print(people)
+
+def findPerson(person):
     x = 0
 
-    for person in people:
-        if(person == 'Waldo'):
+    for seat in people:
+        if(seat == person):
             print(person + ' is gevonden!')
             break
         else:
             x += 1
 
-    return x
+    return x + 1
 
-stoel = findWaldo()
+stoel = findPerson('Waldo')
 
 print('Waldo zit op stoel nummer: ' + str(stoel))
